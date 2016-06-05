4d7b9b52c4041c7533e33dc9704d1ed869710289
Josh Kuhar <jdkuhar@gmail.com>
2016-06-05 18:48:05 -0400

    commit message

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 $(".enter").click(function() {
+	var numberInTen = $(".text-box").val();
+    $(".output").empty();
+    while (numberInTen < 10) {
+      		$(".output").append("<div>"+numberInTen+"</div>");
+      		numberInTen++;
+    	}
+	$(".text-box").val("");
+	console.log("check");
+	});
+
+// use enter to add list items
+$('.text-box').keyup(function(e){
+	if(e.keyCode == 13) {
+		//event.preventDefault();
+		$('.enter').click();
+	};
+});	
+
+/*
+$('ul').on('click', '.remove-item-btn', function(event){
+  $(this).parent().remove();
+  console.log("remove clicked");
+});
+
+$(".enter").click(function() {
 	$(".output").empty();
 	$(".output").append($(".text-box").val()); 
 	$(".text-box").val("");
 	console.log("check");
 	});
-/*
-// use enter to add list items
-$('.add-item-input').keyup(function(event){
-	if(event.keyCode == 13) {
-		//event.preventDefault();
-		$('.add-item-button').click();
-	};
-});	
-
-$('ul').on('click', '.remove-item-btn', function(event){
-  $(this).parent().remove();
-  console.log("remove clicked");
-});
 
 */
