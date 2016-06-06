3529981102aa492368502bdcb3a8bf4f2b945af5
Josh Kuhar <jdkuhar@gmail.com>
2016-06-05 20:49:43 -0400

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(".enter").click(function() {
     $(".output").empty();
   
-	var y = $(".text-box").val();
+	var inputNumber = $(".text-box").val();
     var x = 1;
-    while (x <= y) {
+    while (x <= inputNumber) {
       if ((x % 3 === 0) & (x % 5 == 0)){
         $(".output").append("<div>"+"fizz buzz"+"</div>");
       }
@@ -30,17 +30,3 @@ $('.text-box').keyup(function(e){
 	};
 });	
 
-/*
-$('ul').on('click', '.remove-item-btn', function(event){
-  $(this).parent().remove();
-  console.log("remove clicked");
-});
-
-$(".enter").click(function() {
-	$(".output").empty();
-	$(".output").append($(".text-box").val()); 
-	$(".text-box").val("");
-	console.log("check");
-	});
-
-*/
